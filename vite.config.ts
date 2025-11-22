7a7aa1ae73f0c476800bdd3f5a58fe692e6c300a
Confession Stories <confessionstories01@gmail.com>
2025-11-22 13:53:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    // This is needed because the Google GenAI SDK and some other libs
-    // expect 'process.env' to exist, but browsers don't have it.
-    'process.env': {}
-  }
+  plugins: [react()]
 })
